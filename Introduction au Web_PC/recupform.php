38aafe9ab5cade8a1bda2c611eac1f9e228707c3
jeremie dentan <jeredentan@gmail.com>
2015-11-08 19:17:31 +0100

    commit

---
+++ Introduction au Web_PC/recupform.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+                  $nom = $_POST['nom'];
+                  $email = $_POST['email'];
+                  $message = $_POST['message'];
+                  print("$nom  $prenom $message");
+                  echo $nom;
+                  echo $email;
+
+
+?>
+</html>
+
